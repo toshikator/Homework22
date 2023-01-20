809c4ac3e5d1be532b6b94ac12f696a2c5cdf12b
Anton Bukhman <diamondspb2015@gmail.com>
2023-01-20 15:32:45 +0200

    added complete ObjectForAdd class

--- js/ObjectForAdd.js
+++ js/ObjectForAdd.js
@@ -6,13 +6,17 @@ class ObjectForAdd {
     static #productType = this.#formConnector.type.value;
     static #productSpecialParameterConnector = this.#formConnector.special_parameter.value;
     static #specialParametersDatabase = Product.specialParametersDatabase;
-    static #getNameOfSpecialField = ObjectForAdd.#specialParametersDatabase[ObjectForAdd.#productSpecialParameterConnector];
+    // static #getNameOfSpecialField = ObjectForAdd.#specialParametersDatabase[ObjectForAdd.#productSpecialParameterConnector];
     static #newObject = {};
     static get getNewObject(){
         ObjectForAdd.#newObject['title'] = ObjectForAdd.#productTitle;
         ObjectForAdd.#newObject['manufacturer'] = ObjectForAdd.#productManufacturer;
         ObjectForAdd.#newObject['price'] = ObjectForAdd.#productPrice;
-        ObjectForAdd.#newObject[ObjectForAdd.#getNameOfSpecialField] = ObjectForAdd.#productSpecialParameterConnector.value;
+        let valueOfSpecialParameter = 'unknown name of the special field';
+        if (this.#specialParametersDatabase.keys.contains(this.#productType)){
+            valueOfSpecialParameter = ObjectForAdd.#specialParametersDatabase[ObjectForAdd.#productType];
+        }
+        ObjectForAdd.#newObject[ObjectForAdd.#productType] = valueOfSpecialParameter;
         return JSON.parse(JSON.stringify(ObjectForAdd.#newObject));
     }
 
