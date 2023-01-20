10d7d97c63c98277107ed2c9f1e1a2f437ccc20f
Anton Bukhman <diamondspb2015@gmail.com>
2023-01-20 02:21:10 +0200

    with HTML

---
+++ js/script.js
@@ -0,0 +1,31 @@
+
+
+(()=>{
+    const scriptList = [
+        './js/Product.js',
+        './js/Milk.js',
+        './js/Chocolate.js',
+        './js/Wine.js',
+        './js/Store.js',
+        './js/QA.js'
+
+
+
+
+
+    ];
+    scriptList.forEach((value)=>{
+        const script = document.createElement('script');
+        script.setAttribute('src',value);
+        script.defer = false;
+        document.head.appendChild(script);
+        // console.log(script);
+    });
+
+
+
+
+
+
+})()
+
